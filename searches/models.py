9c2e7188a9f44dac9ad215e7127d135c4f4dac87
betoesquivel <ppbeto94@gmail.com>
2014-06-09 14:06:07 -0500

    Added comment to searches

--- searches/models.py
+++ searches/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# This is just in order to get some extra info on the use for the db
 class Search(models.Model):
 	query = models.CharField(max_length=100)
 	requests = models.IntegerField(default=1)
 	author = models.BooleanField(default=False)
 	book = models.BooleanField(default=False)
 	category = models.BooleanField(default=False)
-	
+
 	def __unicode__(self):
 		return self.query
 
