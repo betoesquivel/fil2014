ecdf9fb230b817ae5f1c75b69a98d6c1dda1ea3e
Bernardo Trevino <bernardotc94@gmail.com>
2014-06-10 11:23:13 -0500

    Testing with exhibitors, found errors

--- exhibitors/admin.py
+++ exhibitors/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from exhibitors.models import Exhibitor, Workshop, WorkshopEvents, Stand, Editorial, Book, Author, Category, Award, Volunteer, BookRegistered
 
 # Register your models here.
+admin.site.register(Exhibitor)
+admin.site.register(Workshop)
+admin.site.register(WorkshopEvents)
+admin.site.register(Stand)
+admin.site.register(Editorial)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Award)
+admin.site.register(Volunteer)
+admin.site.register(BookRegistered)
